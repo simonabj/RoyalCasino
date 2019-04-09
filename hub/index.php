6ff7552763564198b328d9fec01d1d0cdc4d68fc
Julian <julian.kragset@gmail.com>
2019-04-09 15:26:56 +0200

    css

---
+++ hub/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+	<title> Casino Royale </title>
+    <link rel="icon" href="https://cdn2.iconfinder.com/data/icons/interface-part-1/32/html-code-512.png">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+	<style>
+
+		#id {}
+
+	</style>
+</head>
+<body>
+<header>
+
+</header>
+
+<div id="documentWrapper">
+
+    
+
+</div>
+<!-------------- JAVASCRIPT -------------->
+<script>
+
+    
+
+</script>
+</body>
+</html>

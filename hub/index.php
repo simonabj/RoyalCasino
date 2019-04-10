028881e18e4ca0c291e8f587fe7b1395c5b130c1
Julian <julian.kragset@gmail.com>
2019-04-10 11:17:59 +0200

    Changes header animation and scrolled size.

--- hub/index.php
+++ hub/index.php
@@ -20,21 +20,34 @@
 
 </head>
 <body>
+<!--<div class="container row centerCross centerMain" style="z-index: 1; height: 50px; width:100vw; top: 10px; position: fixed;">
+    <button class="secondary square">1</button>
+    <button class="secondary square">2</button>
+    <button class="secondary square">3</button>
+    <button class="secondary square">4</button>
+</div>-->
 <header style="position: fixed">
-    <div class="column container">
 
-        <div class="titleDiv" style="border-radius: 500px; min-height: 50px;">
+
+    <div class="column container div">
+        <div class="titleDiv shape-bat blackText div" style="border-radius: 0; min-height: 50px; top: 10px;">
             <h1>
                 Casino Royale!
             </h1>
         </div>
 
-        <span class="shape-diamond" style="border-bottom-color: var(--secondary); display: flex; top: 85px"></span>
+        <span class="bordershape-diamond"
+              style="border-bottom-color: var(--secondary); display: flex; top: 85px"></span>
         <!-- alternative: when scrolling, turns into a triangle pointing up. clicking it returns the user to the top of the page. -->
+        <!-- TODO: når diamond vises, kan den trykkes på (scale(1.1) on hover & 1.2@click). Da skyter toolbar menyen ut fra begge sider av den.
+                To tabs til venstre, og to til høyre, og når man scroller dem vekk eller trykker på diamanten igjen så glir de tilbake inn i diamanten og skjules,
+                og diamanten spiller av en liten animasjon mens den mottar tabs'ene, satisfying qubic-bezier rotateX/Y spin(ning til de er inne).-->
     </div>
 
 </header>
 <div id="documentWrapper" class="container column" style="margin-top: 300px !important;">
+
+
 
 
     <h1>test</h1>
@@ -201,10 +214,13 @@
         // When the user scrolls down 50px from the top of the document, resize the header's font size
         // alternative - ha en vanlig square header, men når bruker scroller ned, bytt class til bookmarkesque triangle header & make small.
         let header = document.querySelector("header");
-        let div = header.querySelector("div");
-        let text = div.querySelector("h1");
+        let div = header.getElementsByClassName("titleDiv")[0];
         let symbol = header.querySelector("span");
-        let toggled = false;
+        let headerToggled = false;
+        let headerTransitionDuration = 0.5;
+        header.style.transitionDuration = headerTransitionDuration + "s";
+        div.style.transitionDuration = headerTransitionDuration + "s";
+        symbol.style.transitionDuration = headerTransitionDuration + "s";
 
 
         /**
@@ -212,14 +228,12 @@
          */
         function resizeHeaderOnScroll() {
             let distanceY = window.pageYOffset || document.documentElement.scrollTop;
-            console.log(distanceY);
-            console.log(toggled);
             let shrinkOn = 200;
 
-            if (distanceY > shrinkOn && toggled === false) {
-                toggled = true;
-                console.log("enabling");
+            if (distanceY > shrinkOn && headerToggled === false) {
+                headerToggled = true;
 
+                //symbol.classList.add("spin");
                 header.style.fontSize = "10px";
                 symbol.style.width = 0;
                 symbol.style.height = 0;
@@ -227,35 +241,41 @@
                 symbol.style.opacity = 0;
 
                 setTimeout(function () {
-                    header.style.clipPath = "polygon(0 0, 100% 0, 50vw 60%, 0 calc(50% - 50%))";
+                    header.style.clipPath = "polygon(0 0, 100% 0, 50vw 40%, 0 calc(50% - 50%))";
                     header.style.padding = 0;
                     header.style.height = "50px";
-                    div.style.height = "50px";
-                    div.style.top = "10px";
-                }, 500);
+
+                    div.style.top = "-25px";
+                    div.classList.remove("shape-bat");
+                    div.classList.add("shape-bat-top");
+
+                }, 250);
 
 
-            } else if (distanceY < shrinkOn && toggled) {
-                toggled = false;
-                console.log("disabling");
-                    header.style.clipPath = "polygon(0 0, 100% 0, 50vw 100%, 0 calc(50% - 50%))";
-                    header.style.padding = "initial";
-                    header.style.height = "initial";
-                    div.style.height = "initial";
+            } else if (distanceY < shrinkOn && headerToggled) {
+                headerToggled = false;
+                //symbol.classList.remove("spin");
+                header.style.clipPath = "polygon(0 0, 100% 0, 50vw 100%, 0 calc(50% - 50%))";
+                header.style.padding = "initial";
+                header.style.height = "initial";
+                div.style.height = "initial";
+                div.classList.remove("shape-bat-top");
+                div.classList.add("shape-bat");
 
                 setTimeout(function () {
-                div.style.position = "fixed";
-                    div.style.top = "35px";
+                    div.style.position = "fixed";
+                    div.style.top = "10px";
 
                     header.style.fontSize = "initial";
                     symbol.style.width = 0;
                     symbol.style.height = 0;
                     symbol.style.borderWidth = "50px";
                     symbol.style.opacity = 1;
-                }, 500);
+                }, 250);
 
             }
         }
+
 
         // When the window is scrolled, triggers resizeHeaderOnScroll().
         window.onscroll = function () {resizeHeaderOnScroll()};

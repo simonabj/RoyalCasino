f5322b64da0490817fc21262bf41d2562a900b55
Nomissimon10 <Simon.lyngar@gmail.com>
2019-04-11 12:39:48 +0200

    Lagt inn kommentarer og fikset småfeil i kode.

--- pullOut.php
+++ pullOut.php
@@ -1,29 +1,33 @@
 <head>
     <title>Counter</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> <!-- Koble til jQuery -->
 </head>
 <body>
 
-<p><span id="ganger">1.00</span>x</p>
+<p><span id="ganger">1.00</span>x</p> <!-- Det som viser verdien til ganger tallet -->
 
-<button id="button">Start</button>
-<button id="button2">Stopp</button>
-<input type="number" id="bet" placeholder="Hvor mye vedder du?" />
+<button id="button">Start</button> <!-- Knapp for å starte bettet -->
+<button id="button2">Pull Out</button> <!-- Knapp for å gi seg med profitt -->
+<input type="number" id="bet" placeholder="Hvor mye vedder du?" /> <!-- Input for hvor mye penger du vedder -->
 <p id="utfall"></p>
 
 <script>
+    /*Lage variabler for å kunne bruke dem senere i spillet*/
     var gangerEl=document.querySelector("#ganger");
     var utfallEl=document.querySelector("#utfall");
     var buttonEl=document.querySelector("#button");
     var button2El=document.querySelector("#button2");
     var betEl=document.querySelector("#bet");
+    /*Event listeners for å kjøre funksjonene*/
     buttonEl.addEventListener("click", rullNummer);
     buttonEl.addEventListener("click", sluttVerdiFunksjon);
+    /*Verdiene man trenger i oppgaven*/
     var n=1;
     var ganger2=1.00;
     var sluttVerdi;
     var betSatt;
 
+    /*Funksjonen for å sette sluttverdien og ordne event listerne så man ikke kan gjenta flere prosesser*/
     function sluttVerdiFunksjon() {
         betSatt=betEl.value;
         sluttVerdi=1+0.01*Math.floor(Math.random() * 375)+0.25;
@@ -34,6 +38,7 @@
         button2El.addEventListener("click", pullOut);
     }
 
+    /*Funksjon for å øke tallet, og hvos tallet når slutt tallet stopper den og du taper*/
     function rullNummer() {
         if (ganger2>=sluttVerdi) {
             buttonEl.addEventListener("click", rullNummer);
@@ -45,10 +50,11 @@
             ganger2=1+0.01*n;
             gangerEl.innerHTML=ganger2.toFixed(2);
             n++;
-            setTimeout(rullNummer, (300-0.4*n));
+            setTimeout(rullNummer, (300-0.4*n)); /*Gjenta prosessen, øk tallet raskere og raskere*/
         }
     }
 
+    /*Funksjon for å stoppe prosessen og tjene tokens til kontoen din*/
     function pullOut() {
         buttonEl.removeEventListener("click", pullOut);
         utfallEl.innerHTML="Du trakk deg ut i tide, veddet "+betSatt+" og tjente "+betSatt*ganger2+" Tokens. Innsatsen din ble ganget med "+ganger2+".";

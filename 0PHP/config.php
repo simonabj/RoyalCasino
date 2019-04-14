f85a89cbdeffc5ea183e1f8794aae60e93d6ef8c
Simon <49441210+Nomissimon10@users.noreply.github.com>
2019-04-14 23:11:24 +0200

    Comments added

--- 0PHP/config.php
+++ 0PHP/config.php
@@ -1,11 +1,11 @@
 <?php
 /*Definering av alle passord og connection til database, dette vil være til hjelp senere*/
-define('DB_SERVER', 'mysql.hostinger.com');
-define('DB_USERNAME', 'u201393012_cr');
-define('DB_PASSWORD', '1EjjQpVKmAMa');
-define('DB_NAME', 'u201393012_cr');
+define('DB_SERVER', 'mysql.hostinger.com'); /*Definering av server*/
+define('DB_USERNAME', 'u201393012_cr'); /*Definering av brukernavn*/
+define('DB_PASSWORD', '1EjjQpVKmAMa'); /*Definering av passord*/
+define('DB_NAME', 'u201393012_cr'); /*Definering av navn*/
 
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME); /*Definere connection til database som variablen $link*/
 
 if (!$link->set_charset("utf8")) {
     printf("", $link->error);

0448f69d78a9ab8e05f5d4cd45543aed39a4cd6f
Simon <49441210+Nomissimon10@users.noreply.github.com>
2019-04-16 21:50:04 +0200

    Added file

---
+++ 0PHP/reset-password (1).php
@@ -0,0 +1,91 @@
+<?php
+session_start();
+
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+
+require_once "config.php";
+
+$new_password = $confirm_password = "";
+$new_password_err = $confirm_password_err = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    if(empty(trim($_POST["new_password"]))){
+        $new_password_err = "Please enter the new password.";
+    } elseif(strlen(trim($_POST["new_password"])) < 6){
+        $new_password_err = "Password must have atleast 6 characters.";
+    } else{
+        $new_password = trim($_POST["new_password"]);
+    }
+
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm the password.";
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($new_password_err) && ($new_password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+
+    if(empty($new_password_err) && empty($confirm_password_err)){
+        $sql = "UPDATE users SET password = ? WHERE id = ?";
+
+        if($stmt = mysqli_prepare($link, $sql)){
+            mysqli_stmt_bind_param($stmt, "si", $param_password, $param_id);
+
+            $param_password = password_hash($new_password, PASSWORD_DEFAULT);
+            $param_id = $_SESSION["id"];
+
+            if(mysqli_stmt_execute($stmt)){
+                session_destroy();
+                header("location: login.php");
+                exit();
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+
+        mysqli_stmt_close($stmt);
+    }
+
+    mysqli_close($link);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>VIDRATES | Reset Password</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper" style="text-align:center;text-align-last:center; margin-left:auto;margin-right:auto">
+        <h2>Reset Password</h2>
+        <p>Fyll ut dette sjemaet for å endre passordet ditt.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($new_password_err)) ? 'has-error' : ''; ?>">
+                <label>Nytt Passord</label>
+                <input type="password" name="new_password" class="form-control" value="<?php echo $new_password; ?>">
+                <span class="help-block"><?php echo $new_password_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+                <label>Bekreft Passordet</label>
+                <input type="password" name="confirm_password" class="form-control">
+                <span class="help-block"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Submit">
+                <a class="btn btn-link" href="innstillinger.php">Avbryt</a>
+            </div>
+        </form>
+    </div>
+</body>
+</html>

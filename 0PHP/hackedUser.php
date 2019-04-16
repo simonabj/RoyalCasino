1fc7ad8b6090e283e687b7981220d09bb6c88f19
BuildTools <unconfigured@null.spigotmc.org>
2019-04-16 02:44:29 +0200

    Comments

--- 0PHP/hackedUser.php
+++ 0PHP/hackedUser.php
@@ -1,24 +1,29 @@
 <?php
 session_start(); /*Start Session*/
 
-require_once "config.php";
+require_once "config.php"; /*Hent konfigurasjonsfilen*/
+
+/*Ta en sjekk på om brukeren er logget inn, hvis ikke skal brukeren tas til login siden*/
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
 }
 
-$tilkobling = mysqli_connect("mysql.hostinger.com", "u201393012_cr", "1EjjQpVKmAMa", "u201393012_cr");
+$tilkobling = mysqli_connect("mysql.hostinger.com", "u201393012_cr", "1EjjQpVKmAMa", "u201393012_cr"); /*Kobling til databasen*/
 
+/*Sette siden til utf8*/
 if (!$tilkobling->set_charset("utf8")) {
     printf("", $tilkobling->error);
 } else {
     printf("", $tilkobling->character_set_name());
 }
 
+/*Definere session variablene til lettere bruk senere i filen*/
 $seUser = $_SESSION["username"];
 $seBrukerID = $_SESSION["id"];
 $seMail = $_SESSION["mail"];
 
+/*Generering av et 6 tegn langt passord*/
 $new_password = "";
 $length = 6;
 $characters = array_merge(range('A','Z'), range('a','z'), range('0','9'));
@@ -28,6 +33,7 @@ for ($i = 0; $i < $length; $i++) {
 	$new_password .= $characters[$rand];
 }
 
+/*Sending av mail om nytt passord*/
 ini_set( 'display_errors', 1 );
 error_reporting( E_ALL );
 $from = "vidrate@rateateachernorway.com";
@@ -37,16 +43,18 @@ $message = "Hey, $seUser you have just registered that someone else has logged i
 $headers = "From:" . $from;
 mail($to,$subject,$message, $headers);
 
+/*Kryptering av nytt passord*/
 $hashedPassword = password_hash($new_password, PASSWORD_DEFAULT);
 
+/*Spørresetning som setter nytt passord i databasen*/
 $sql = sprintf("UPDATE users SET password='%s' WHERE id = '%s'",
   $tilkobling->real_escape_string($hashedPassword),
   $tilkobling->real_escape_string($seBrukerID)
 );
-$tilkobling->query($sql);
+$tilkobling->query($sql); /*Utførelse av nytt passord*/
 
 $_SESSION = array();
-session_destroy();
+session_destroy(); /*Ødelegg session så man må logge inn igjen*/
 ?>
 
 <!DOCTYPE html>
@@ -54,9 +62,8 @@ session_destroy();
 <head>
     <meta charset="UTF-8">
     <title>Casino Royale | User Compromised</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <link href="stilark.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css"> <!-- CSS Eksternt stilark -->
+    <link href="stilark.css" rel="stylesheet" type="text/css"> <!-- Lokalt stilark CSS -->
     <style>
         body{
             font: 14px sans-serif;
@@ -65,13 +72,11 @@ session_destroy();
         h1 {
             color: white;
         }
-        .fa, .fas, .far {
-            font-weight: 1000;
-        }
     </style>
 </head>
 <body>
 
+<!-- Informere brukeren om hva som skjer nå utifra hva som er registrert-->
 <h2 style="width:99%; text-align: left; margin-left:auto; margin-right:auto; font-size:20px; color:#337ab7;">Administratorer har nå blitt advart om at noen har logget seg inn på brukeren din, sjekk mailen din for et nytt passord. Husk å endre passordet ditt til et nytt et etter innlogging. (Finner du ikke mailen? Sjekk spam mappen)</a></h2>
 
 </body>

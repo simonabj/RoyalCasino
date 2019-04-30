5930ef6ffbe6062f4bb88268677875a9f73dde87
Simon Andreas <simon.a@unseen.is>
2019-04-30 17:50:23 +0200

    Update til Subsystem + Docs

--- 0JS/sub/querySQL.php
+++ 0JS/sub/querySQL.php
@@ -2,12 +2,14 @@
 
 require_once($_SERVER['DOCUMENT_ROOT'].'/0PHP/config.php');
 
-$sql = $_POST['query'];
+header('Content-Type: application/json');
+
+$sql = json_decode(file_get_contents('php://input'))->query;
 
 $result = $link->query($sql);
 
 if($result->num_rows > 0) {
-    echo json_encode($result->fetch_all());
+    echo json_encode($result->fetch_assoc());
 } else {
     return $link->error;
 }

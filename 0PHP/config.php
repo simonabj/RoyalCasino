4ecf5fe578da54abdc05b5a2cd6fcec00ec6d824
Nomissimon10 <Simon.lyngar@gmail.com>
2019-04-30 14:14:03 +0200

    Fix Config file

--- 0PHP/config.php
+++ 0PHP/config.php
@@ -1,6 +1,6 @@
 <?php
 /*Definering av alle passord og connection til database, dette vil være til hjelp senere*/
-define('DB_SERVER', 'localhost'); /*Definering av server*/
+define('DB_SERVER', 'localhost'); /*Definering av server */
 define('DB_USERNAME', 'u201393012_cr'); /*Definering av brukernavn*/
 define('DB_PASSWORD', '123321hhh'); /*Definering av passord*/
 define('DB_NAME', 'u201393012_cr'); /*Definering av navn*/

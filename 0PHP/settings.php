c7b25b0dfad9d29dbc7963da49c752ab5cc33123
Simon <49441210+Nomissimon10@users.noreply.github.com>
2019-05-02 21:20:18 +0200

    Update settings.php

--- 0PHP/settings.php
+++ 0PHP/settings.php
@@ -26,6 +26,7 @@ $datasett22 = mysqli_query($link, $sql3); /*Datasett*/
     <link href="stilark.css" rel="stylesheet" type="text/css"> <!-- Stilark -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> <!-- jQuery -->
     
+    <script src="/0JS/RoyaleSubsystem.js"></script> <!-- Subsystemet -->
     <link href="../../0CSS/universal.css" rel="stylesheet"> <!-- Meny øverst til høyre -->
     <script src="../../0JS/universal_menu.js"></script> <!-- Meny øverst til høyre -->
     <link href="../../0CSS/universal_menu.css" rel="stylesheet"> <!-- Meny øverst til høyre -->
